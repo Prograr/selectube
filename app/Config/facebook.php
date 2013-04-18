03927d248f67a08cbc3939bb7e0361bea2ed1064
Florian Ajir <>
2013-04-18 02:58:42 +0200

    ajout fichier de config facebook

---
+++ app/Config/facebook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+  * Get an api_key and secret from facebook and fill in this content.
+  * save the file to app/Config/facebook.php
+  */
+  $config = array(
+  	'Facebook' => array(
+  		'appId' => '448385471909603',
+  		'apiKey' => '448385471909603',
+  		'secret' => '1fa9d5b6cf8dc5a22a7a8c4936768431',
+  		'cookie' => true,
+  		'locale' => 'fr_FR',
+  		)
+  	);
+?>
