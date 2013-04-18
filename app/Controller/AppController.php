c035e4b612aebde4154514e39f69f55439c78748
Florian Ajir <>
2013-04-18 02:29:16 +0200

    facebook connect

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Application level Controller
  *
@@ -32,10 +33,12 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
 //    public $theme = "Cakestrap";
-    public $helpers = array('Html', 'Form', 'Html2');
+    public $helpers = array('Html', 'Form', 'Html2', 'Facebook.Facebook' => array('locale' => 'fr_FR'));
     public $components = array(
         'Session',
+        'Facebook.Connect',
         'Auth' => array(
             'authenticate' => array(
                 'Form' => array(
@@ -54,8 +57,37 @@ class AppController extends Controller {
     public function beforeFilter() {
         $this->Auth->allow('index', 'voir', 'display', 'accueil');
 //        $this->Auth->authorize("display");
+        //Get all the details on the facebook user
+        $this->set('facebookUser', $this->Connect->user());
+        $this->set('user', $this->Auth->user());
+
+        //retrieve only the id from the facebook user
+        $this->set('facebook_id', $this->Connect->user('id'));
+
+        //retrieve only the email from the facebook user
+        $this->set('facebook_email', $this->Connect->user('email'));
+        $this->set('facebook_prenom', $this->Connect->user('first_name'));
+        $this->set('facebook_nom', $this->Connect->user('last_name'));
+        try {
+            $user = $this->Auth->user();
+            $fb_user = $this->Connect->user();
+        } catch (FacebookApiException $e) {
+            
+        }
+        if ($fb_user = $this->Connect->user()) {
+            if (!$this->Auth->loggedIn()) {
+                if (!isset($fb_user['error_code'])) {
+                    $this->loadModel('User');
+                    $this->User->recursive = -1;
+                    if (($user = $this->User->findByFacebookId($fb_user['id'])) ||
+                            ($user = $this->User->findByEmail($fb_user['email']))) {
+                        $this->Auth->login($user);
+                    }
+                }
+            }
+        }
     }
-    
+
     public function isAuthorized($user) {
         // Admin peut accéder à toute action
         if (isset($user['role']) && $user['role'] === 'admin') {
@@ -65,4 +97,21 @@ class AppController extends Controller {
         // Refus par défaut
         return false;
     }
+
+    //Add an email field to be saved along with creation.
+    function beforeFacebookSave() {
+        $this->Connect->authUser['User']['email'] = $this->Connect->user('email');
+        return true; //Must return true or will not save.
+    }
+
+    function beforeFacebookLogin($user) {
+        //Logic to happen before a facebook login
+//        debug($user);
+    }
+
+    function afterFacebookLogin() {
+        //Logic to happen after successful facebook login.
+        $this->redirect($this->Auth->loginRedirect);
+    }
+
 }
